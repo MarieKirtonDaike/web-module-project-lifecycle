922b56bde2d33965998b938b7fbb431523d7af09
Emrie <emrie@emries-mbp.mynetworksettings.com>
2023-04-12 21:11:34 -0400

    added the axios

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,9 +1,41 @@
 import React from 'react'
+import TodoList from "./TodoList"
+import axios from "axios"
+
 
 const URL = 'http://localhost:9000/api/todos'
 
 export default class App extends React.Component {
-  render() {
-    return null
+    state = {
+      todos: [],
+    }
+
+  fetchalltodos = () =>{
+    axios.get(URL)
+    .then(res=>{
+      this.setState({...this.state, todos: res.data.data})
+    })
+    .catch(err =>{debugger})
+  };
+
+  componentDidMount(){
+    this.fetchalltodos()
   }
+  
+  render() {
+    return (
+      <>
+        <h1>TO DO </h1>
+        <ul>
+          {this.state.todos.map(td=> {return (<li key={td.id}>{td.name}</li>)})}
+        </ul>
+        <form >
+          <input  type="text" />
+          <button >ADD TODO</button>
+          <button> CLEAR COMPLETED</button>
+        </form>
+      </>
+    )
+  }
+
 }
